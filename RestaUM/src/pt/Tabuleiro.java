a4c1c47ed9bd4dbb9a44f4e9e313da6eff9d903a
MatheusASCandido <63605727+MatheusASCandido@users.noreply.github.com>
2020-04-14 21:27:42 -0300

    Update Tabuleiro.java

--- RestaUM/src/pt/Tabuleiro.java
+++ RestaUM/src/pt/Tabuleiro.java
@@ -1,9 +1,11 @@
 package pt;
-
+//Matheus A. S. Candido - ra241640
+//Pedro H. Bueno - ra223402
+//classe parte da etapa resta um do trabalho
 public class Tabuleiro {
-    Pecas tab [][];
+    Pecas tab [][];//uma matriz de objetos do tipo pecas
    
-    void criar_tab(int a) {
+    void criar_tab(int a) {//cria os objetos na matriz
         this.tab = new Pecas [a][a];
         for (int i=0; i<7; i++){
             for (int j=0; j<7; j++){
@@ -12,7 +14,7 @@ public class Tabuleiro {
         }
     }
    
-    void fazer_tab(){
+    void fazer_tab(){//inicializa os objetos
         for (int i=0; i<2; i++){
             for (int j=0; j<7; j++){
                 if (j>1 && j<5){
@@ -41,7 +43,7 @@ public class Tabuleiro {
         tab[3][3].comer();
     }
    
-    void mostrar_tab(){
+    void mostrar_tab(){//mostra o tabuleiro apos cada movimento
         for (int i=0; i<7; i++){
             System.out.print((7-i)+" ");
             for (int j=0; j<7; j++){
@@ -53,7 +55,7 @@ public class Tabuleiro {
         System.out.println();
     }
    
-    void mostrar_tab_inicial(){
+    void mostrar_tab_inicial(){//mostra o tabuleiro no inicio
         System.out.println("Tabuleiro inicial:");
         for (int i=0; i<7; i++){
             System.out.print((7-i)+" ");
@@ -66,13 +68,14 @@ public class Tabuleiro {
         System.out.println();
     }
    
-    void comando(String a){
-        char b = a.charAt(0);
-        char c = a.charAt(1);
-        char d = a.charAt(3);
-        char e = a.charAt(4);
+    void comando(String a){//executa o comando x0:x0, x representa letras e 0 numeros
+        char b = a.charAt(0);//tranforma a letra de inicio em um caracter
+        char c = a.charAt(1);// transforma o numero de inicio em um caracter
+        char d = a.charAt(3);// transforma a letra de destino em um caracter
+        char e = a.charAt(4);// transforma o numero de destino em um caracter
         int n = 0;
         int m = 0;
+        //transforma as letras de inicio e destino em um numero referente a matriz que representa o tabuleiro
         if (b == 'a'){
             n=0;
         }
@@ -116,9 +119,10 @@ public class Tabuleiro {
         if (d == 'g'){
             m=6;
         }
+        //transforma o caracter numerico do comando em um numero referente a matriz
         int i = Character.getNumericValue(c);
         int j = Character.getNumericValue(e);
-       
+       //faz o movimento do comando
         tab[7-(i)][n].comer();
         tab[7-(j)][m].comer();
         int meio=0;
