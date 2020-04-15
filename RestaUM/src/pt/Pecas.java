4f922fab3aa813c21d653e7f768c6d267ca93054
MatheusASCandido <63605727+MatheusASCandido@users.noreply.github.com>
2020-04-14 21:29:04 -0300

    Update Pecas.java

--- RestaUM/src/pt/Pecas.java
+++ RestaUM/src/pt/Pecas.java
@@ -1,5 +1,7 @@
 package pt;
-
+//Matheus A. S. Candido - ra241640
+//Pedro H. Bueno - ra223402
+//classe parte da etapa resta um do trabalho
 public class Pecas {//classe de pecas
     String n;//representa a peca sendo "p" para uma peca existente, "-" um espaco vazio no tabuleiro ou " " para um espaco nao existente no tabuleiro
    
